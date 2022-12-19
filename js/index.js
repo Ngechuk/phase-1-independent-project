c5826e1cfe8ce745082e63b1f41ab42cf80c7b5e
Kevin <kelvinngechu@gmail.com>
2022-12-19 15:23:52 +0300

    adding jokes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
 
 const button = document.getElementById("generate-joke");
-const jokeDiv = document.getElementById("joke");
+const loadCategories = async () => {
+    respJson = await getJson(chuckCategories);
+  
+    let dropDown = document.querySelector("#joke-category");
+    dropDown.options.innerHTML = "";
+  
 
 button.addEventListener("click", () => {
   fetch("https://api.chucknorris.io/jokes/random")
     .then(response => response.json())
     .then(data => {
       jokeDiv.innerHTML = data.value;
+      console.log(data.value);
     });
 });
+fetch("https://api.chucknorris.io/jokes/search?query=chuck", {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.result);
+    });
+  
